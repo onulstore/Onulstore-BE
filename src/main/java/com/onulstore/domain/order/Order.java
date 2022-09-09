f92b05d539f64ab5209c02f1c101bf5e0a6ce50d
leewg97 <leewg97@naver.com>
2022-09-10 03:07:13 +0900

    #4 feat : Add Order - Order Entity 작성 - Order Repository 작성 - OrderStatus enum class 작성

---
+++ src/main/java/com/onulstore/domain/order/Order.java
@@ -0,0 +1,40 @@
+package com.onulstore.domain.order;
+
+import com.onulstore.common.BaseTimeEntity;
+import com.onulstore.domain.enums.OrderStatus;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Orders")
+public class Order extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String orderNumber;
+
+    @Column
+    private String orderName;
+
+    @Column
+    private String deliveryMessage;
+
+    @Column
+    private String address;
+
+    @Column
+    private Integer amount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+}
---
+++ src/main/java/com/onulstore/domain/order/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.onulstore.domain.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
