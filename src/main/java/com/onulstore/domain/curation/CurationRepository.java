484ef603412f1084c34a5a8244c9df355a93fdea
leewg97 <leewg97@naver.com>
2022-09-10 03:03:36 +0900

    #4 feat : Add Curation - Curation Entity & Curation Repository 작성

---
+++ src/main/java/com/onulstore/domain/curation/CurationRepository.java
@@ -0,0 +1,6 @@
+package com.onulstore.domain.curation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurationRepository extends JpaRepository<Curation, Long> {
+}
