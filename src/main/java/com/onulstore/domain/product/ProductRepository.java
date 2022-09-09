d3f9c01d2fa501a713d4e1ebb6e90da5a090d9af
leewg97 <leewg97@naver.com>
2022-09-10 02:46:10 +0900

    #4 feat : Add ProductRepository

---
+++ src/main/java/com/onulstore/domain/product/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.onulstore.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
