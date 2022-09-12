2d43807112307da7ff071637dd7f32b373031c0d
leewg97 <leewg97@naver.com>
2022-09-12 18:30:17 +0900

    feat : Add security related files - Add UserDetailsService - Add SecurityConfig - Add SecurityUtil

---
+++ src/main/java/com/onulstore/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.onulstore.config;
+
+import com.onulstore.jwt.JwtAccessDeniedHandler;
+import com.onulstore.jwt.JwtAuthenticationEntryPoint;
+import com.onulstore.jwt.JwtSecurityConfig;
+import com.onulstore.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CorsFilter corsFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .addFilter(corsFilter)
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/",  "/**", "/auth/**", "/products", "/v2/api-docs", "/swagger-resources/**"
+                        , "/swagger-ui.html", "/swagger-ui/index.html", "/webjars/**", "/swagger/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        return http.build();
+    }
+}
