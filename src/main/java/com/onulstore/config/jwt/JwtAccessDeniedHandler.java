9123fab281ec8063fe61007804085f2e7c1bce22
leewg97 <leewg97@naver.com>
2022-09-13 01:18:43 +0900

    rename : Change directories

--- src/main/java/com/onulstore/config/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/onulstore/config/jwt/JwtAccessDeniedHandler.java
@@ -1,4 +1,4 @@
-package com.onulstore.jwt;
+package com.onulstore.config.jwt;
 
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
--- src/main/java/com/onulstore/config/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/onulstore/config/jwt/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,4 @@
-package com.onulstore.jwt;
+package com.onulstore.config.jwt;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
