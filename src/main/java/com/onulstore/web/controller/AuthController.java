7e2282294b257ee4984d33a07ca6ca0f0a033277
leewg97 <leewg97@naver.com>
2022-09-13 19:43:04 +0900

    chore : 오타 수정

--- src/main/java/com/onulstore/web/controller/AuthController.java
+++ src/main/java/com/onulstore/web/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-@Api(tags = {"Auth Controller"})
+@Api(tags = {"Auth-Controller"})
 public class AuthController {
 
     private final AuthService authService;
