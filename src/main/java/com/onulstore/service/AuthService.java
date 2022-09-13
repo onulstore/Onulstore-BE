3100416ee4bde4663c097c9d404791623ec04a6e
leewg97 <leewg97@naver.com>
2022-09-13 19:39:39 +0900

    feat : Update AuthService - 관리자 회원가입(임시) - 전체 회원 조회 로직 추가

--- src/main/java/com/onulstore/service/AuthService.java
+++ src/main/java/com/onulstore/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.onulstore.service;
 
+import com.onulstore.config.SecurityUtil;
+import com.onulstore.config.jwt.TokenProvider;
+import com.onulstore.domain.enums.Authority;
 import com.onulstore.domain.member.Member;
 import com.onulstore.domain.member.MemberRepository;
-import com.onulstore.config.jwt.TokenProvider;
+import com.onulstore.exception.AccessPrivilegeExceptions;
+import com.onulstore.exception.NotExistUserException;
 import com.onulstore.web.dto.LoginDto;
 import com.onulstore.web.dto.MemberDto;
 import com.onulstore.web.dto.TokenDto;
@@ -13,6 +17,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +35,7 @@ public class AuthService {
         if (memberRepository.existsByEmail(signupRequest.getEmail())) {
             throw new RuntimeException("이미 존재하는 이메일입니다.");
         }
+
         Member member = signupRequest.toMember(passwordEncoder);
         return MemberDto.MemberResponse.of(memberRepository.save(member));
     }
@@ -38,6 +46,35 @@ public class AuthService {
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
         return tokenProvider.generateToken(authentication);
+    }
+
+    // 관리자 회원가입
+    @Transactional
+    public MemberDto.MemberResponse admin(MemberDto.AdminRequest adminRequest) {
+        if (memberRepository.existsByEmail(adminRequest.getEmail())) {
+            throw new RuntimeException("이미 존재하는 이메일입니다.");
+        }
+
+        Member member = adminRequest.toMember(passwordEncoder);
+        return MemberDto.MemberResponse.of(memberRepository.save(member));
+    }
+
+    // 전체 회원 조회
+    @Transactional(readOnly = true)
+    public HashMap<String, Object> viewAllMember() {
+        HashMap<String, Object> resultMap = new HashMap<>();
+
+        Member member = memberRepository.findById(SecurityUtil.getCurrentMemberId()).orElseThrow(
+                () -> new NotExistUserException("존재하지 않는 유저입니다."));
+
+        if (!member.getAuthority().equals(Authority.ROEL_ADMIN)) {
+            throw new AccessPrivilegeExceptions("접근 권한이 없습니다.");
+        }
+
+        List<Member> allMemberList = memberRepository.findAll();
+        resultMap.put("viewAllMember", allMemberList);
+
+        return resultMap;
     }
 
 }
