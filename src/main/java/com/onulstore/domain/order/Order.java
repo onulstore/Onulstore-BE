f0ad98f3146fe7546ed631eba9537a2032a5744e
leewg97 <leewg97@naver.com>
2022-09-16 17:50:57 +0900

    feat : Modify Order Entity

--- src/main/java/com/onulstore/domain/order/Order.java
+++ src/main/java/com/onulstore/domain/order/Order.java
@@ -1,51 +1,70 @@
 package com.onulstore.domain.order;
 
 import com.onulstore.common.BaseTimeEntity;
-import com.onulstore.domain.cart.Cart;
 import com.onulstore.domain.enums.OrderStatus;
 import com.onulstore.domain.member.Member;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "Orders")
+@NoArgsConstructor
 public class Order extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    private String orderNumber;
-
-    @Column
-    private String orderName;
-
-    @Column
-    private String deliveryMessage;
-
-    @Column
-    private String address;
-
-    @Column
-    private Integer amount;
-
-    @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-    private List<Cart> carts = new ArrayList<>();
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
 
+    private LocalDateTime orderDate;
+
+    @Column
+    private String orderStatus;
+
+    public void addOrderProduct(OrderProduct orderProduct) {
+        orderProducts.add(orderProduct);
+        orderProduct.setOrder(this);
+    }
+
+    public static Order createOrder(Member member, List<OrderProduct> orderProductList) {
+        Order order = new Order();
+        order.setMember(member);
+        for (OrderProduct orderProduct : orderProductList) {
+            order.addOrderProduct(orderProduct);
+        }
+        order.setOrderDate(LocalDateTime.now());
+        order.setOrderStatus(OrderStatus.COMPLETE.getKey());
+        return order;
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+
+        for (OrderProduct orderProduct : orderProducts) {
+            totalPrice += orderProduct.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
+    public void orderCancel() {
+
+        this.orderStatus = OrderStatus.CANCEL.getKey();
+        for (OrderProduct orderProduct : orderProducts) {
+            orderProduct.cancel();
+        }
+    }
 }
