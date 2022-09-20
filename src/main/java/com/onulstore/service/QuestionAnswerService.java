d29039a28ce5d67abd99f7f0386c60710af41e7b
army246 <jar4608@gmail.com>
2022-09-20 22:04:55 +0900

    feat: 답변 등록

---
+++ src/main/java/com/onulstore/service/QuestionAnswerService.java
@@ -0,0 +1,41 @@
+package com.onulstore.service;
+
+import com.onulstore.config.SecurityUtil;
+import com.onulstore.domain.member.Member;
+import com.onulstore.domain.member.MemberRepository;
+import com.onulstore.domain.question.Question;
+import com.onulstore.domain.question.QuestionRepository;
+import com.onulstore.domain.questionAnswer.QuestionAnswer;
+import com.onulstore.domain.questionAnswer.QuestionAnswerRepository;
+import com.onulstore.exception.NotExistUserException;
+import com.onulstore.web.dto.QuestionAnswerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionAnswerService {
+
+    private final QuestionAnswerRepository questionAnswerRepository;
+    private final MemberRepository memberRepository;
+    private final QuestionRepository questionRepository;
+
+    // 답변 등록
+    @Transactional
+    public void insertAnswer(Long questionId, QuestionAnswerDto questionAnswerDto) {
+        Member member = memberRepository.findById(SecurityUtil.getCurrentMemberId()).orElseThrow(
+                () -> new NotExistUserException("존재하지 않는 유저입니다."));
+        Question question = questionRepository.findById(questionId).orElseThrow(() -> new NotExistUserException("등록되지 않은 질문입니다."));
+
+        QuestionAnswer answer = QuestionAnswer.builder()
+                .member(member)
+                .question(question)
+                .answer(questionAnswerDto.getAnswer())
+                .build();
+
+        questionAnswerRepository.save(answer);
+        question.Answered();
+    }
+
+}
