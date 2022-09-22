4d174b7d6b7528aacc23ebc31151569f865f05cc
leewg97 <leewg97@naver.com>
2022-09-23 00:36:27 +0900

    feat : Modify Curation Entity - CurationProduct 삭제 후 엔티티 하나로 통합 - 그에따른 매거진, 추천상품 등록 로직 변경

--- src/main/java/com/onulstore/domain/curation/Curation.java
+++ src/main/java/com/onulstore/domain/curation/Curation.java
@@ -3,13 +3,10 @@ package com.onulstore.domain.curation;
 import com.onulstore.common.BaseTimeEntity;
 import com.onulstore.domain.enums.CurationForm;
 import com.onulstore.domain.member.Member;
+import com.onulstore.domain.product.Product;
 import lombok.*;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -22,40 +19,49 @@ public class Curation extends BaseTimeEntity {
     private Long id;
 
     @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    private String curationImg;
+
+    @Column
     private String curationForm;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "curation", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<CurationProduct> curationProducts = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 
-    public void addCurationProduct(CurationProduct curationProduct) {
-        curationProducts.add(curationProduct);
-        curationProduct.setCuration(this);
+    public static Curation createCurationM(String title, String content, String curationImg,
+                                           Member member, Product product) {
+        Curation curation = new Curation();
+        curation.setTitle(title);
+        curation.setContent(content);
+        curation.setCurationImg(curationImg);
+        curation.setCurationForm(CurationForm.MAGAZINE.getKey());
+        curation.setMember(member);
+        curation.setProduct(product);
+
+        return curation;
     }
 
-    // Magazine 등록
-    public static Curation createCurationM(Member member, List<CurationProduct> curationProducts) {
-        Curation curationM = new Curation();
-        curationM.setMember(member);
-        for (CurationProduct curationProduct : curationProducts) {
-            curationM.addCurationProduct(curationProduct);
-        }
-        curationM.setCurationForm(CurationForm.MAGAZINE.getKey());
-        return curationM;
-    }
+    public static Curation createCurationR(String title, String content, String curationImg,
+                                           Member member, Product product) {
+        Curation curation = new Curation();
+        curation.setTitle(title);
+        curation.setContent(content);
+        curation.setCurationImg(curationImg);
+        curation.setCurationForm(CurationForm.RECOMMEND.getKey());
+        curation.setMember(member);
+        curation.setProduct(product);
 
-    // Recommend 등록
-    public static Curation createCurationR(Member member, List<CurationProduct> curationProducts) {
-        Curation curationR = new Curation();
-        curationR.setMember(member);
-        for (CurationProduct curationProduct : curationProducts) {
-            curationR.addCurationProduct(curationProduct);
-        }
-        curationR.setCurationForm(CurationForm.RECOMMEND.getKey());
-        return curationR;
+        return curation;
     }
 
 }
