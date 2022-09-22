a687e2108558f9cbc0c28ea7b8e171c18f5e074d
leewg97 <leewg97@naver.com>
2022-09-22 21:16:12 +0900

    feat : Add Notice Controller

---
+++ src/main/java/com/onulstore/web/controller/NoticeController.java
@@ -0,0 +1,64 @@
+package com.onulstore.web.controller;
+
+import com.onulstore.service.NoticeService;
+import com.onulstore.web.dto.NoticeDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/notices")
+@Api(tags = {"Notice-Controller"})
+public class NoticeController {
+
+    private final NoticeService noticeService;
+
+    @PostMapping
+    @ApiOperation(value = "공지 등록")
+    public ResponseEntity<NoticeDto.NoticeResponse> addNotice(@RequestBody NoticeDto.NoticeRequest noticeRequest) {
+        return ResponseEntity.ok(noticeService.addNotice(noticeRequest));
+    }
+
+    @PutMapping("/{noticeId}")
+    @ApiOperation(value = "공지 수정")
+    public ResponseEntity<NoticeDto.NoticeResponse> updateNotice(
+            @RequestBody NoticeDto.NoticeRequest noticeRequest, @PathVariable Long noticeId) {
+        return ResponseEntity.ok(noticeService.updateNotice(noticeRequest, noticeId));
+    }
+
+    @GetMapping("/{noticeId}")
+    @ApiOperation(value = "해당 공지 조회")
+    public ResponseEntity<NoticeDto.NoticeResponse> getNotice(@PathVariable Long noticeId) {
+        return ResponseEntity.ok(noticeService.getNotice(noticeId));
+    }
+
+    @GetMapping
+    @ApiOperation(value = "전체 공지 조회")
+    public ResponseEntity<HashMap<String, Object>> getNoticeList() {
+        return ResponseEntity.ok(noticeService.getNoticeList());
+    }
+
+    @DeleteMapping("/{noticeId}")
+    @ApiOperation(value = "공지 삭제")
+    public ResponseEntity<String> deleteNotice(@PathVariable Long noticeId) {
+        noticeService.deleteNotice(noticeId);
+        return ResponseEntity.ok("공지가 삭제되었습니다.");
+    }
+
+    @PostMapping("/{noticeId}/image")
+    @ApiOperation(value = "공지 이미지 업로드")
+    public ResponseEntity<String> uploadImage(
+            @RequestParam("images") MultipartFile multipartFile, @PathVariable Long noticeId) throws IOException {
+        String image = noticeService.upload(multipartFile.getInputStream(), multipartFile.getOriginalFilename());
+        noticeService.addImage(noticeId, image);
+        return ResponseEntity.ok("이미지가 등록되었습니다.");
+    }
+
+}
