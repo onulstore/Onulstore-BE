64c93117c347e246622d8cee73bbff01c5fd61d0
leewg97 <leewg97@naver.com>
2022-09-22 21:18:13 +0900

    feat : Add Custom Exception - WISHLIST_ALREADY_EXIST 추가

--- src/main/java/com/onulstore/domain/enums/UserErrorResult.java
+++ src/main/java/com/onulstore/domain/enums/UserErrorResult.java
@@ -21,8 +21,8 @@ public enum UserErrorResult implements SuperErrorResult{
   LOGOUT_USER(HttpStatus.BAD_REQUEST,"로그아웃 된 사용자입니다."),
   NOT_EXIST_QUESTION(HttpStatus.NOT_FOUND, "질문을 찾을 수 없습니다."),
   NOT_EXIST_ANSWER(HttpStatus.NOT_FOUND, "질문에 대한 답글을 찾을 수 없습니다."),
-  WISHLIST_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 찜 등록이 되어있습니다.");
-
+  WISHLIST_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 찜 등록이 되어있습니다."),
+  NOT_FOUND_NOTICE(HttpStatus.NOT_FOUND, "해당 공지를 찾을 수 없습니다.");
 
   private final HttpStatus httpStatus;
   private final String message;
