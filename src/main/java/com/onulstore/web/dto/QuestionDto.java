5c8c0fc6ffec19cd7dedf46d4f695e7d9fd1461a
army246 <jar4608@gmail.com>
2022-09-23 19:53:07 +0900

    feat: Modify QuestionDto

--- src/main/java/com/onulstore/web/dto/QuestionDto.java
+++ src/main/java/com/onulstore/web/dto/QuestionDto.java
@@ -12,7 +12,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class QuestionDto {
 
-    private Long memberId;
     private Long productId;
     private String title;
     private String content;
@@ -20,7 +19,6 @@ public class QuestionDto {
 
     public static QuestionDto of (Question question) {
         return QuestionDto.builder()
-                .memberId(question.getMember().getId())
                 .productId(question.getProduct().getId())
                 .title(question.getTitle())
                 .content(question.getContent())
