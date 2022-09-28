33e43c0e7adfbc1d7d18dfbc6e766e11802a4c84
diaboloss217 <diaboloss217@gmail.com>
2022-09-29 02:28:13 +0900

    refactor: coupon enums

--- src/main/java/com/onulstore/domain/enums/DiscountType.java
+++ src/main/java/com/onulstore/domain/enums/DiscountType.java
@@ -1,6 +1,15 @@
 package com.onulstore.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum DiscountType {
-  PERCENT,
-  TOTAL
+
+  PERCENT("PERCENT", "퍼센트 할인"),
+  TOTAL("TOTAL", "총액 할인");
+
+  private final String key;
+  private final String title;
 }
