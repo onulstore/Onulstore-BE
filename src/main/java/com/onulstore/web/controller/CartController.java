489e05656e12d8df7c68f19b5c126751db091365
diaboloss217 <diaboloss217@gmail.com>
2022-09-28 15:43:31 +0900

    refactor: cartService - plus minus button

--- src/main/java/com/onulstore/web/controller/CartController.java
+++ src/main/java/com/onulstore/web/controller/CartController.java
@@ -1,11 +1,9 @@
 package com.onulstore.web.controller;
 
-import com.onulstore.domain.member.Member;
 import com.onulstore.domain.member.MemberRepository;
 import com.onulstore.service.CartService;
 import com.onulstore.web.dto.CartDto;
 import io.swagger.annotations.Api;
-import io.swagger.models.Response;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -44,12 +42,12 @@ public class CartController {
 
   @PostMapping("/carts/{cartId}/plus")
   public ResponseEntity<CartDto> plusQuantity(@PathVariable Long cartId) {
-    return ResponseEntity.ok(cartService.plus(cartId));
+    return ResponseEntity.ok(cartService.plusOne(cartId));
   }
 
   @PostMapping("/carts/{cartId}/minus")
   public ResponseEntity<CartDto> minusQuantity(@PathVariable Long cartId) {
-    return ResponseEntity.ok(cartService.minus(cartId));
+    return ResponseEntity.ok(cartService.minusOne(cartId));
   }
 
 }
