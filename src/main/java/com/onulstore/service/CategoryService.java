2b088c91af0e76b0290e926f09a097f84e2c6c3b
leewg97 <leewg97@naver.com>
2022-10-04 03:17:00 +0900

    #98 feat : Modify CategoryService - 카테고리 아이디로 상품 조회 로직 추가(2depth 까지) - 카테고리 조회 리턴 값 HashMap -> Map

--- src/main/java/com/onulstore/service/CategoryService.java
+++ src/main/java/com/onulstore/service/CategoryService.java
@@ -7,21 +7,26 @@ import com.onulstore.domain.enums.Authority;
 import com.onulstore.domain.enums.UserErrorResult;
 import com.onulstore.domain.member.Member;
 import com.onulstore.domain.member.MemberRepository;
+import com.onulstore.domain.product.Product;
+import com.onulstore.domain.product.ProductRepository;
 import com.onulstore.exception.UserException;
 import com.onulstore.web.dto.CategoryDto;
+import com.onulstore.web.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class CategoryService {
 
+    private final ProductRepository productRepository;
     private final MemberRepository memberRepository;
     private final CategoryRepository categoryRepository;
 
@@ -30,11 +35,11 @@ public class CategoryService {
      * @return 카테고리 전체 정보
      */
     @Transactional(readOnly = true)
-    public HashMap<String, Object> getCategories() {
-        HashMap<String, Object> resultMap = new HashMap<>();
+    public Map<String, List<Category>> getCategories() {
+        Map<String, List<Category>> resultMap = new HashMap<>();
 
         List<Category> findAllCategory = categoryRepository.findAll();
-        resultMap.put("readAll", findAllCategory);
+        resultMap.put("findAllCategory", findAllCategory);
         return resultMap;
     }
 
@@ -90,6 +95,30 @@ public class CategoryService {
                 () -> new UserException(UserErrorResult.CATEGORY_NOT_FOUND));
         categoryRepository.delete(category);
     }
+
+    /**
+     * categoryId로 상품 조회
+     * @param categoryId
+     * @param pageable
+     * @return categoryId로 조회한 상품 목록(2depth 까지)
+     */
+    public Page<ProductDto.ProductResponse> getCategoryById(Long categoryId, Pageable pageable) {
+        Category findCategory = categoryRepository.findById(categoryId).orElseThrow(
+                () -> new UserException(UserErrorResult.CATEGORY_NOT_FOUND));
+
+        List<ProductDto.ProductResponse> findProductByCategory = new ArrayList<>();
+        List<Category> findAllCategory = categoryRepository.findAllByParentId(categoryId);
+        findAllCategory.add(findCategory);
+        for (Category category : findAllCategory) {
+            Product product = productRepository.findByCategoryId(category.getId()).orElseThrow();
+            findProductByCategory.add(ProductDto.ProductResponse.of(product));
+        }
+
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), findAllCategory.size());
+        return new PageImpl<>(findProductByCategory.subList(start, end), pageable, findProductByCategory.size());
+    }
+
 }
 
 
