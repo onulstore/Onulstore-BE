10bf4d447a466529f1ccc98bc75a2dba64ac9cbe
leewg97 <leewg97@naver.com>
2022-10-04 03:15:50 +0900

    #98 feat : Modify CategoryController - 카테고리 아이디로 상품 조회 로직 추가(2depth 까지) - HashMap -> Map으로 변경

--- src/main/java/com/onulstore/web/controller/CategoryController.java
+++ src/main/java/com/onulstore/web/controller/CategoryController.java
@@ -1,16 +1,23 @@
 package com.onulstore.web.controller;
 
+import com.onulstore.domain.category.Category;
 import com.onulstore.service.CategoryService;
 import com.onulstore.web.dto.CategoryDto;
+import com.onulstore.web.dto.ProductDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/categories")
@@ -22,7 +29,7 @@ public class CategoryController {
 
     @GetMapping
     @ApiOperation(value = "모든 카테고리 조회")
-    public ResponseEntity<HashMap<String, Object>> getCategories() {
+    public ResponseEntity<Map<String, List<Category>>> getCategories() {
         return ResponseEntity.ok(categoryService.getCategories());
     }
 
@@ -47,6 +54,14 @@ public class CategoryController {
     public ResponseEntity<String> deleteCategory(@ApiParam(required = true) @PathVariable Long categoryId) {
         categoryService.deleteCategory(categoryId);
         return ResponseEntity.ok("카테고리가 삭제되었습니다.");
+    }
+
+    @GetMapping("/{categoryId}/products")
+    @ApiOperation(value = "카테고리 아이디로 상품 조회(2depth 까지)")
+    public ResponseEntity<Page<ProductDto.ProductResponse>> getCategoryById(
+            @ApiParam(required = true) @PathVariable Long categoryId,
+            @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size=8) Pageable pageable) {
+        return ResponseEntity.ok(categoryService.getCategoryById(categoryId, pageable));
     }
 
 }
