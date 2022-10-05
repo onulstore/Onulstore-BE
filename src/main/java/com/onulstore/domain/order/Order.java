3ad02ade6013f6f081855b2d881fe87e979ac0bc
leewg97 <leewg97@naver.com>
2022-10-06 01:07:02 +0900

    #103 feat : Modify Order Entity - 주문 내역에 들어갈 내용 추가

--- src/main/java/com/onulstore/domain/order/Order.java
+++ src/main/java/com/onulstore/domain/order/Order.java
@@ -23,6 +23,44 @@ public class Order extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String firstKana;
+
+    @Column
+    private String lastKana;
+
+    @Column
+    private String postalCode;
+
+    @Column
+    private String roadAddress;
+
+    @Column
+    private String buildingName;
+
+    @Column
+    private String detailAddress;
+
+    @Column
+    private String phoneNum;
+
+    @Column
+    private String email;
+
+    @Column
+    private String deliveryMessage;
+    @Column
+    private String orderStatus;
+
+    private LocalDateTime orderDate;
+
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
@@ -30,22 +68,39 @@ public class Order extends BaseTimeEntity {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<OrderProduct> orderProducts = new ArrayList<>();
 
-    private LocalDateTime orderDate;
-
-    @Column
-    private String orderStatus;
 
     public void addOrderProduct(OrderProduct orderProduct) {
         orderProducts.add(orderProduct);
         orderProduct.setOrder(this);
     }
 
-    public static Order createOrder(Member member, List<OrderProduct> orderProductList) {
+    public static Order createCartOrder(Member member, List<OrderProduct> orderProductList) {
         Order order = new Order();
         order.setMember(member);
         for (OrderProduct orderProduct : orderProductList) {
             order.addOrderProduct(orderProduct);
         }
+        order.setOrderDate(LocalDateTime.now());
+        order.setOrderStatus(OrderStatus.COMPLETE.getKey());
+        return order;
+    }
+
+    public static Order createOrder(Member member, String deliveryMessage,
+        OrderProduct orderProduct) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setEmail(member.getEmail());
+        order.setLastKana(member.getLastKana());
+        order.setFirstKana(member.getFirstKana());
+        order.setLastName(member.getLastName());
+        order.setFirstName(member.getFirstName());
+        order.setPhoneNum(member.getPhoneNum());
+        order.setPostalCode(member.getPostalCode());
+        order.setRoadAddress(member.getRoadAddress());
+        order.setBuildingName(member.getBuildingName());
+        order.setDetailAddress(member.getDetailAddress());
+        order.setDeliveryMessage(deliveryMessage);
+        order.addOrderProduct(orderProduct);
         order.setOrderDate(LocalDateTime.now());
         order.setOrderStatus(OrderStatus.COMPLETE.getKey());
         return order;
