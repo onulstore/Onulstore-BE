823c8ad86cb62ab634dc0a4339afc61e47575f08
leewg97 <leewg97@naver.com>
2022-10-06 23:39:57 +0900

    #122 feat : Add OrderProductRepository

---
+++ src/main/java/com/onulstore/domain/order/OrderProductRepository.java
@@ -0,0 +1,9 @@
+package com.onulstore.domain.order;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+
+    List<OrderProduct> findAllByOrderId(Long orderId);
+}
