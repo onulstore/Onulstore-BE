90f9b5e311fef09d3108ba6c1a16c8990943761e
leewg97 <leewg97@naver.com>
2022-10-06 23:50:57 +0900

    #103 refactor : Modify OrderStatus - 주문 상태 이름 변경

--- src/main/java/com/onulstore/domain/enums/OrderStatus.java
+++ src/main/java/com/onulstore/domain/enums/OrderStatus.java
@@ -11,8 +11,8 @@ public enum OrderStatus {
     COMPLETE("COMPLETE", "주문 완료"),
     REFUND_REQUEST("REFUND_REQUEST", "환불 요청"),
     REFUND_COMPLETE("REFUND_COMPLETE", "환불 완료"),
-    WAIT_PAYMENT("WAIT_PAYMENT", "결제 대기"),
-    COMPLETE_PAYMENT("COMPLETE_PAYMENT", "결제 완료"),
+    PAYMENT_WAIT("PAYMENT_WAIT", "결제 대기"),
+    PAYMENT_COMPLETE("PAYMENT_COMPLETE", "결제 완료"),
     PURCHASE_CONFIRM("PURCHASE_CONFIRM", "구매 확정");
 
     private final String key;
