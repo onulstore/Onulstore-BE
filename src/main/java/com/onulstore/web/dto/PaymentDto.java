77af879138cfeaed52b0c86a24715fa0b3b99987
leewg97 <leewg97@naver.com>
2022-10-06 23:41:02 +0900

    #122 feat : Add PaymentDto

---
+++ src/main/java/com/onulstore/web/dto/PaymentDto.java
@@ -0,0 +1,52 @@
+package com.onulstore.web.dto;
+
+import com.onulstore.domain.enums.PaymentMeasure;
+import com.onulstore.domain.payment.Payment;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+public class PaymentDto {
+
+    @Getter
+    @Setter
+    public static class PaymentRequest {
+
+        private Long orderId;
+        private Long couponId;
+        private Integer mileage;
+        private Integer deliveryPrice;
+        @ApiModelProperty(value = "결제 방법", required = true, example = "CREDIT_CARD/ACCOUNT/STORE_PAY")
+        private PaymentMeasure paymentMeasure;
+
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @AllArgsConstructor
+    public static class PaymentResponse {
+
+        private PaymentMeasure paymentMeasure;
+        private Integer productPrice;
+        private Integer discount;
+        private Integer deliveryPrice;
+        private Integer acquirePoint;
+        private Integer paymentAmount;
+
+        public static PaymentResponse of(Payment payment) {
+            return PaymentResponse.builder()
+                .paymentMeasure(payment.getPaymentMeasure())
+                .productPrice(payment.getProductPrice())
+                .discount(payment.getDiscount())
+                .deliveryPrice(payment.getDeliveryPrice())
+                .acquirePoint(payment.getAcquirePoint())
+                .paymentAmount(payment.getPaymentAmount())
+                .build();
+        }
+
+    }
+
+}
