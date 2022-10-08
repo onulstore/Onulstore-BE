45188d846f69b325ba3cb12a887b044065f44cc2
leewg97 <leewg97@naver.com>
2022-10-09 07:54:43 +0900

    #137 feat : Modify OrderRepository

--- src/main/java/com/onulstore/domain/order/OrderRepository.java
+++ src/main/java/com/onulstore/domain/order/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.onulstore.domain.order;
 
 import com.onulstore.domain.enums.OrderStatus;
+import com.onulstore.domain.member.Member;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,13 +14,17 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select o from Order o " +
-            "where o.member.email = :email " +
-            "order by o.orderDate desc")
+        "where o.member.email = :email " +
+        "order by o.orderDate desc")
     List<Order> findOrders(@Param("email") String email, Pageable pageable);
 
     @Query("select count(o) from Order o " +
-            "where o.member.email = :email")
+        "where o.member.email = :email")
     Long countOrder(@Param("email") String email);
 
-    List<Order> findAllByOrderStatusAndCreatedDateAfter(OrderStatus purchaseConfirm, LocalDateTime localDateTime);
+    List<Order> findAllByOrderStatusAndCreatedDateAfter(OrderStatus purchaseConfirm,
+        LocalDateTime localDateTime);
+
+    Optional<Order> findByIdAndMember(Long orderId, Member member);
+
 }
