85ec5ff163a5c1aae33be97538ab3af0f17ddd6c
leewg97 <leewg97@naver.com>
2022-10-09 07:57:03 +0900

    #137 feat : Add ReviewImageRepository

---
+++ src/main/java/com/onulstore/domain/review/ReviewImageRepository.java
@@ -0,0 +1,7 @@
+package com.onulstore.domain.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
+
+}
