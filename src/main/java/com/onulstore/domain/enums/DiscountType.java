b2d176bb1aca340b5ad7856fd2ab9345eb13f0db
diaboloss217 <diaboloss217@gmail.com>
2022-10-12 01:18:08 +0900

    feat : add enum

--- src/main/java/com/onulstore/domain/enums/DiscountType.java
+++ src/main/java/com/onulstore/domain/enums/DiscountType.java
@@ -7,9 +7,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum DiscountType {
 
-  PERCENT("PERCENT", "퍼센트 할인"),
-  TOTAL("TOTAL", "총액 할인");
+    NONE("NONE", "할인 없음"),
+    PERCENT("PERCENT", "퍼센트 할인"),
+    TOTAL("TOTAL", "총액 할인");
 
-  private final String key;
-  private final String title;
+    private final String key;
+    private final String title;
 }
