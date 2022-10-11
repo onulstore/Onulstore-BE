21d2e57b5df03126f9610663ee58c3d2cc7cfd18
leewg97 <leewg97@naver.com>
2022-10-11 21:11:05 +0900

    #40 refactor : Modify PrincipalOauth2UserService

--- src/main/java/com/onulstore/config/auth/PrincipalOauth2UserService.java
+++ src/main/java/com/onulstore/config/auth/PrincipalOauth2UserService.java
@@ -18,40 +18,45 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
-	private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
-	@Override
-	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-		OAuth2User oAuth2User = super.loadUser(userRequest);
-		return processOAuth2User(userRequest, oAuth2User);
-	}
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        return processOAuth2User(userRequest, oAuth2User);
+    }
 
-	private OAuth2User processOAuth2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
+    private OAuth2User processOAuth2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
 
-		OAuth2UserInfo oAuth2UserInfo = null;
-		if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
-			oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
-		}
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        }
 
-		Optional<Member> userOptional =
-				memberRepository.findByProviderAndProviderId(oAuth2UserInfo.getProvider(), oAuth2UserInfo.getProviderId());
-		
-		Member member;
-		if (userOptional.isPresent()) {
-			member = userOptional.get();
-			member.setEmail(oAuth2UserInfo.getEmail());
-			memberRepository.save(member);
-		} else {
-			member = Member.builder()
-					.username(oAuth2UserInfo.getProvider() + "_" + oAuth2UserInfo.getProviderId())
-					.email(oAuth2UserInfo.getEmail())
-					.authority("ROLE_USER")
-					.provider(oAuth2UserInfo.getProvider())
-					.providerId(oAuth2UserInfo.getProviderId())
-					.build();
-			memberRepository.save(member);
-		}
+        Optional<Member> userOptional =
+            memberRepository.findByProviderAndProviderId(oAuth2UserInfo.getProvider(),
+                oAuth2UserInfo.getProviderId());
 
-		return new PrincipalDetails(member, oAuth2User.getAttributes());
-	}
+        Member member;
+        if (userOptional.isPresent()) {
+            member = userOptional.get();
+            member.setEmail(oAuth2UserInfo.getEmail());
+            memberRepository.save(member);
+        } else {
+            member = Member.builder()
+                .username(oAuth2UserInfo.getProvider() + "_" + oAuth2UserInfo.getProviderId())
+                .email(oAuth2UserInfo.getEmail())
+                .authority("ROLE_USER")
+                .provider(oAuth2UserInfo.getProvider())
+                .providerId(oAuth2UserInfo.getProviderId())
+                .firstKana(oAuth2UserInfo.getName())
+                .lastKana(oAuth2UserInfo.getName())
+                .firstName(oAuth2UserInfo.getName())
+                .lastName(oAuth2UserInfo.getName())
+                .build();
+            memberRepository.save(member);
+        }
+
+        return new PrincipalDetails(member, oAuth2User.getAttributes());
+    }
 }
