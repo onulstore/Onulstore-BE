6792935273832631d0bbd2f6e0045da447b0353d
diaboloss217 <diaboloss217@gmail.com>
2022-10-13 20:27:00 +0900

    feat : recentOrders

--- src/main/java/com/onulstore/domain/order/OrderRepository.java
+++ src/main/java/com/onulstore/domain/order/OrderRepository.java
@@ -3,13 +3,12 @@ package com.onulstore.domain.order;
 import com.onulstore.domain.enums.OrderStatus;
 import com.onulstore.domain.member.Member;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
@@ -32,4 +31,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     Long countByCreatedDateAfter(LocalDateTime localDateTime);
 
     Long countByOrderStatusAndCreatedDateBetween(OrderStatus orderStatus, LocalDateTime start, LocalDateTime end);
+
+    List<Order> findTop10ByOrderByCreatedDateDesc();
 }
