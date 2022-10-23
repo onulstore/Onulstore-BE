8213f2a4fb2dd22798c86e757fad3dc56da95609
leewg97 <leewg97@naver.com>
2022-10-23 22:48:08 +0900

    refactor : Modify OrderController - Modify typo

--- src/main/java/com/onulstore/web/controller/OrderController.java
+++ src/main/java/com/onulstore/web/controller/OrderController.java
@@ -4,7 +4,9 @@ import com.onulstore.service.OrderService;
 import com.onulstore.web.dto.OrderDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -42,15 +44,15 @@ public class OrderController {
     @PostMapping
     @ApiOperation(value = "단일 상품 주문 생성 / 인증 필요")
     public ResponseEntity<String> addCategory(
-        @Valid @RequestBody OrderDto.OrderRequest orderRequest) {
+            @Valid @RequestBody OrderDto.OrderRequest orderRequest) {
         orderService.createOrder(orderRequest);
         return ResponseEntity.ok("상품 주문이 완료되었습니다.");
     }
 
-    @PostMapping("/cartorder")
+    @PostMapping("/cartOrder")
     @ApiOperation(value = "장바구니 선택 상품 주문 생성 / 인증 필요")
     public ResponseEntity<String> CreateOrderList(
-        @RequestBody OrderDto.CartOrderRequest cartOrderRequest) {
+            @RequestBody OrderDto.CartOrderRequest cartOrderRequest) {
         orderService.createSelectedCartOrder(cartOrderRequest);
         return ResponseEntity.ok("상품 주문이 완료되었습니다.");
     }
@@ -65,7 +67,7 @@ public class OrderController {
     @PutMapping
     @ApiOperation(value = "주문 상태 변경(환불 요청 or 구매 확정) / 인증 필요")
     public ResponseEntity<OrderDto.StatusResponse> updateStatus(
-        @Valid @RequestBody OrderDto.StatusRequest request) {
+            @Valid @RequestBody OrderDto.StatusRequest request) {
         return ResponseEntity.ok(orderService.updateStatus(request));
     }
 
@@ -79,7 +81,7 @@ public class OrderController {
     @PutMapping("/update")
     @ApiOperation(value = "해당 주문 회원 정보 변경 / 인증 필요")
     public ResponseEntity<String> orderRefund(
-        @RequestBody OrderDto.UpdateOrderRequest updateOrderRequest) {
+            @RequestBody OrderDto.UpdateOrderRequest updateOrderRequest) {
         orderService.orderModification(updateOrderRequest);
         return ResponseEntity.ok("해당 주문 정보 수정이 완료되었습니다.");
     }
