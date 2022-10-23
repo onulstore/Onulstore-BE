f0e96712479e0d425d3d904caccf58ee00734b8e
leewg97 <leewg97@naver.com>
2022-10-23 22:49:17 +0900

    refactor : Modify AdminController - Modify typo

--- src/main/java/com/onulstore/web/controller/AdminController.java
+++ src/main/java/com/onulstore/web/controller/AdminController.java
@@ -14,9 +14,11 @@ import com.onulstore.web.dto.OrderDto;
 import com.onulstore.web.dto.RequestTimeDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,7 +43,7 @@ public class AdminController {
     @ApiOperation(value = "입점사 회원가입")
     @PostMapping("/signup")
     public ResponseEntity<MemberDto.MemberResponse> sellerRegistration(
-        @RequestBody MemberDto.SellerRequest request) {
+            @RequestBody MemberDto.SellerRequest request) {
         return ResponseEntity.ok(authService.sellerRegistration(request));
     }
 
@@ -51,104 +53,104 @@ public class AdminController {
         return ResponseEntity.ok().body(authService.viewAllMember());
     }
 
-    @ApiOperation(value = "대쉬보드 전체 / 인증 필요(관리자)")
+    @ApiOperation(value = "대시보드 전체 / 인증 필요(관리자)")
     @PostMapping("/dashboard")
     public ResponseEntity<DashboardDto> getEntireDashboard(
-        @RequestBody RequestTimeDto requestTimeDto) {
+            @RequestBody RequestTimeDto requestTimeDto) {
         LocalDateTime localDateTime = requestTimeDto.getLocalDateTime();
         DashboardDto.DashboardMemberResponse memberDashBoard = memberService.memberDashBoard(
-            localDateTime);
+                localDateTime);
         DashboardDto.DashboardProductResponse productDashBoard = productService.productDashBoard(
-            localDateTime);
+                localDateTime);
         DashboardDto.DashboardCategoryResponse categoryDashboard = orderService.salesByCategory(
-            localDateTime);
+                localDateTime);
         DashboardDto.TotalSaleAmounts totalSaleAmounts = orderService.salesAmount(localDateTime);
         Long order = orderService.orderDashBoard(localDateTime);
         Long question = questionService.questionDashBoard(localDateTime);
         Long review = reviewService.reviewDashBoard(localDateTime);
         DashboardDto.CustomerPosts customerPosts = DashboardDto.CustomerPosts.builder()
-            .orders(order)
-            .questions(question)
-            .reviews(review)
-            .build();
+                .orders(order)
+                .questions(question)
+                .reviews(review)
+                .build();
         DashboardDto.PaidAndDeliveredOrders paidAndDeliveredOrders = orderService.paidAndDeliveredOrders(
-            requestTimeDto.getLocalDateTime());
+                requestTimeDto.getLocalDateTime());
         DashboardDto.DailyStatistic dailyStatistic = orderService.dailyOrderStatistic(
-            requestTimeDto.getLocalDateTime(), requestTimeDto.getOrderStatus());
+                requestTimeDto.getLocalDateTime(), requestTimeDto.getOrderStatus());
         List<OrderDto.OrderResponse> orderResponse = orderService.recentOrders();
 
         DashboardDto dashboard = DashboardDto.builder()
-            .dashboardMemberResponse(memberDashBoard)
-            .dashboardProductResponse(productDashBoard)
-            .dashboardCategoryResponse(categoryDashboard)
-            .totalSaleAmounts(totalSaleAmounts)
-            .customerPosts(customerPosts)
-            .paidAndDeliveredOrders(paidAndDeliveredOrders)
-            .dailyStatistic(dailyStatistic)
-            .orderResponseList(orderResponse)
-            .build();
+                .dashboardMemberResponse(memberDashBoard)
+                .dashboardProductResponse(productDashBoard)
+                .dashboardCategoryResponse(categoryDashboard)
+                .totalSaleAmounts(totalSaleAmounts)
+                .customerPosts(customerPosts)
+                .paidAndDeliveredOrders(paidAndDeliveredOrders)
+                .dailyStatistic(dailyStatistic)
+                .orderResponseList(orderResponse)
+                .build();
 
         return ResponseEntity.ok(dashboard);
     }
 
-    @ApiOperation(value = "대쉬보드 회원 / 인증 필요(관리자)")
+    @ApiOperation(value = "대시보드 회원 / 인증 필요(관리자)")
     @PostMapping("/dashboard/members")
     public ResponseEntity<DashboardDto.DashboardMemberResponse> getMemberInfo(
-        @RequestBody RequestTimeDto requestTimeDto) {
+            @RequestBody RequestTimeDto requestTimeDto) {
         return ResponseEntity.ok(memberService.memberDashBoard(requestTimeDto.getLocalDateTime()));
     }
 
-    @ApiOperation(value = "대쉬보드 상품 / 인증 필요(관리자)")
+    @ApiOperation(value = "대시보드 상품 / 인증 필요(관리자)")
     @PostMapping("/dashboard/products")
     public ResponseEntity<DashboardDto.DashboardProductResponse> getProductInfo(
-        @RequestBody RequestTimeDto requestTimeDto) {
+            @RequestBody RequestTimeDto requestTimeDto) {
         return ResponseEntity.ok(
-            productService.productDashBoard(requestTimeDto.getLocalDateTime()));
+                productService.productDashBoard(requestTimeDto.getLocalDateTime()));
     }
 
-    @ApiOperation(value = "대쉬보드 카테고리 / 인증 필요(관리자)")
+    @ApiOperation(value = "대시보드 카테고리 / 인증 필요(관리자)")
     @PostMapping("/dashboard/categories")
     public ResponseEntity<DashboardDto.DashboardCategoryResponse> getCategoryInfo(
-        @RequestBody RequestTimeDto requestTimeDto) {
+            @RequestBody RequestTimeDto requestTimeDto) {
         return ResponseEntity.ok(orderService.salesByCategory(requestTimeDto.getLocalDateTime()));
     }
 
-    @ApiOperation(value = "대쉬보드 판매금액 아이템 수 / 인증 필요(관리자)")
+    @ApiOperation(value = "대시보드 판매금액 아이템 수 / 인증 필요(관리자)")
     @PostMapping("/dashboard/orders")
     public ResponseEntity<DashboardDto.TotalSaleAmounts> getOrderInfo(
-        @RequestBody RequestTimeDto requestTimeDto) {
+            @RequestBody RequestTimeDto requestTimeDto) {
         return ResponseEntity.ok(orderService.salesAmount(requestTimeDto.getLocalDateTime()));
     }
 
-    @ApiOperation(value = "대쉬보드 질문과 리뷰 / 인증 필요(관리자)")
+    @ApiOperation(value = "대시보드 질문과 리뷰 / 인증 필요(관리자)")
     @PostMapping("dashboard/customers")
     public ResponseEntity<DashboardDto.CustomerPosts> getCustomerPostInfo(
-        @RequestBody RequestTimeDto requestTimeDto) {
+            @RequestBody RequestTimeDto requestTimeDto) {
         Long orders = orderService.orderDashBoard(requestTimeDto.getLocalDateTime());
         Long questions = questionService.questionDashBoard(requestTimeDto.getLocalDateTime());
         Long reviews = reviewService.reviewDashBoard(requestTimeDto.getLocalDateTime());
         DashboardDto.CustomerPosts customerPosts = DashboardDto.CustomerPosts.builder()
-            .orders(orders)
-            .questions(questions)
-            .reviews(reviews)
-            .build();
+                .orders(orders)
+                .questions(questions)
+                .reviews(reviews)
+                .build();
         return ResponseEntity.ok(customerPosts);
     }
 
-    @ApiOperation(value = "대쉬보드 주문 / 인증 필요(관리자)")
-    @PostMapping("dashboard/paymentorders")
+    @ApiOperation(value = "대시보드 주문 / 인증 필요(관리자)")
+    @PostMapping("dashboard/paymentOrders")
     public ResponseEntity<PaidAndDeliveredOrders> getOrderAmounts(
-        @RequestBody RequestTimeDto requestTimeDto) {
+            @RequestBody RequestTimeDto requestTimeDto) {
         return ResponseEntity.ok(
-            orderService.paidAndDeliveredOrders(requestTimeDto.getLocalDateTime()));
+                orderService.paidAndDeliveredOrders(requestTimeDto.getLocalDateTime()));
     }
 
-    @ApiOperation(value = "대쉬보드 통계 / 인증 필요(관리자)")
-    @PostMapping("dashboard/dailystatistic")
+    @ApiOperation(value = "대시보드 통계 / 인증 필요(관리자)")
+    @PostMapping("dashboard/dailyStatistic")
     public ResponseEntity<DashboardDto.DailyStatistic> getDailyStatistic(
-        @RequestBody RequestTimeDto requestTimeDto) {
+            @RequestBody RequestTimeDto requestTimeDto) {
         return ResponseEntity.ok(
-            orderService.dailyOrderStatistic(requestTimeDto.getLocalDateTime(),
-                requestTimeDto.getOrderStatus()));
+                orderService.dailyOrderStatistic(requestTimeDto.getLocalDateTime(),
+                        requestTimeDto.getOrderStatus()));
     }
 }
